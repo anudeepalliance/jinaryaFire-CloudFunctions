6d8506965619c0385af09601554423a6b2f9f0b1
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-07 15:44:19 +0530

    sample

---
+++ functions/src/index.ts
@@ -0,0 +1,39 @@
+import * as functions from 'firebase-functions'
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+    return admin.firestore().collection('userFunction').doc(user.uid).set( {
+        email: user.email,
+        followers: [],
+    })
+  })
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete( user => {
+    const doc = admin.firestore().collection('userFunction').doc(user.uid)
+    return doc.delete()
+})
+
+// http callable function (adding a request)
+exports.sendFeedback = functions.https.onCall((data, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        'unauthenticated', 
+        'only authenticated users can add requests'
+      )
+    }
+    if (data.text.length > 30) {
+      throw new functions.https.HttpsError(
+        'invalid-argument', 
+        'request must be no more than 30 characters long'
+      )
+    }
+    return admin.firestore().collection('feedback').add({
+      Feedback : data.text,
+      uid: context.auth.uid
+    })
+  })
+
