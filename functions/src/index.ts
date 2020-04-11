daa4820a0fefc26629748a11ea0baad7b6a6b623
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-11 23:36:55 +0530

    progress

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,41 +9,36 @@ export { newUserSignUp } from './userCreated'
 export { userDeleted } from './userDeleted'
 // export { newFollowerGained1 } from './newFollowerGained'
 
-exports.newFollowerGained1 = functions.region('asia-east2').firestore
-.document('Users/{userId}/FollowingPeople/{FolloweePersonUid}')
-.onCreate((snap, context) => {
 
-    //Need followee's uid to decide which user's followers sub-collection to update
-    const followeePersonUid = context.params.FolloweePersonUid
+//(1)-----------------------------------------------------------------------------------------------------------------------------------
+//Add Follower Function: This  takes in a Follower User and a Followee Uid 
+//to add a Follower to the Followee's follower sub collection
+exports.newFollowerGained = functions.region('asia-east2').https.onCall((data, context) => {
     
-    //get the follower details
-    const followerPersonUid = context.params.userId
-    const followerPersonName = context.params.name
-    // const followerPersonProfilePhotoChosen = context.params.profilePhotoChosen
-    // const followerPersonUserName = context.params.userName
-  
+  if (!context.auth) {
+      throw new functions.https.HttpsError(
+        'unauthenticated', 
+        'only authenticated users can add requests'
+      );
+    }
+
+    const followerPersonName = data.text
+    const followerPersonUserName = data.userName
+    const followerPersonProfilePhotoChosen = data.profilePhotoChosen
+    const followerPersonUid = context.auth.uid
+    const followeePersonUid = data.followeePersonUid
 
     return admin.firestore()
     .collection('Users').doc(`${followeePersonUid}`)
     .collection('Followers').doc(`${followerPersonUid}`)
     .set( {
       name: followerPersonName,
-      userName: "followerPersonUserName",
-      profilePhotoChosen: "followerPersonProfilePhotoChosen",
+      userName: followerPersonUserName,
+      profilePhotoChosen: followerPersonProfilePhotoChosen,
       uid: followerPersonUid
-    })
-
-      // const followerName = snap.data().age
-      
-      // const followerUserName = newValue.userName
-      // const followerUid = personUid
-      // const followerProfilePhotoChosen = newValue.profilePhotoChosen
-
-      // // access a particular field as you would any JS property
-      // const name = newValue.name;
-
-      // perform desired operations ...
-    })
+    });
+  
+})
 
 
 //(1)-----------------------------------------------------------------------------------------------------------------------------------
