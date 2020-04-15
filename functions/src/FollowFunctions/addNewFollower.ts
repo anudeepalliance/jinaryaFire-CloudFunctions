3a7064af41eb378c0045fc7dd8c98d0019a790df
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-15 18:38:57 +0530

    kl

--- functions/src/FollowFunctions/addNewFollower.ts
+++ functions/src/FollowFunctions/addNewFollower.ts
@@ -1,7 +1,9 @@
 import * as functions from 'firebase-functions'
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore'
 const admin = require('firebase-admin')
 
-
+//When client followes a user, it  calls a callable function to add follower(himself)
+//to the followee's follwers arrayField in the UserDoc
 export const newFollowerGained = functions.region('asia-east2').https.onCall((followData, context) => {
     
     if (!context.auth) {
@@ -13,11 +15,27 @@ export const newFollowerGained = functions.region('asia-east2').https.onCall((fo
   
       const followerUid = context.auth.uid
       const followeeUid = followData.followeeUid
-  
-      return admin.firestore()
-      .collection('Users').doc(followeeUid)
-      .update({
-        followers: admin.firestore.FieldValue.arrayUnion(followerUid)
+
+      const isFollowerBlockedByFollowee = 
+      admin.firestore().collection('Users').doc(followeeUid).collection('ProfileInfo').doc(followeeUid)
+      .where('blocked', 'array-contains', [followerUid])
+      .get().then(snapshot => {
+          if ( !snapshot.exists) {
+              return false
+          } else if ( snapshot.exists ) {
+            return true
+          }
       })
+
+      if ( !isFollowerBlockedByFollowee) {
+        return admin.firestore()
+        .collection('Users').doc(followeeUid)
+        .update({
+          followers: admin.firestore.FieldValue.arrayUnion(followerUid)
+        })
+      } else {
+        return null
+      }
+
     
   })
