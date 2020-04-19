d528ace60b534af5ddf38b65b1828ec9723d2eea
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-19 20:26:18 +0530

    pr

--- functions/src/FollowFunctions/updateUserInfo.ts
+++ functions/src/FollowFunctions/updateUserInfo.ts
@@ -5,7 +5,7 @@ const admin = require('firebase-admin')
 export const onUserDocUpdate = functions.region('asia-east2').firestore.document
 ('Users/{userId}').onUpdate((change, context) => {
 
-    const upDatedUserData = change.after.data()
+const upDatedUserData = change.after.data()
 
 const newName = upDatedUserData?.name
 const profilePhotoChosen = upDatedUserData?.profilePhotoChosen
@@ -13,13 +13,13 @@ const updatersUserId = upDatedUserData?.uid
 const newUserName = upDatedUserData?.userName
 
 const userDoc = change.after.ref.parent
-const followerColl = admin.firestore().collection('Users').document(userDoc).collection("followers")
+const followerColl = admin.firestore().collection('Users').doc(updatersUserId).collection('followers')
 
 //This is where I am stuck, I have the updated document info but how do
 //I find the other documents at firestore that needs updation with this 
 //updated information of the user
 
-return followerColl.get().then(((querySnapshot: { documents: any[] }) => {
+return followerColl.get().then((querySnapshot: { documents: any[] }) => {
     const promises = querySnapshot.documents.map((doc) => {
         const followerUid = doc.id
         return admin.firestore().collection('Users').doc(followerUid).collection('followers')
@@ -31,7 +31,7 @@ return followerColl.get().then(((querySnapshot: { documents: any[] }) => {
         })
     })
     return Promise.all(promises)
-    }))
+    })
 })
 
 
@@ -47,5 +47,3 @@ return followerColl.get().then(((querySnapshot: { documents: any[] }) => {
 //     profilePhotoChosen: profilePhotoChosen,
 //     uid: updatersUserId
 //   })
-
-})
