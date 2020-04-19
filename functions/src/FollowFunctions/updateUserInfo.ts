b8bb210c518efc7ebcc6ddb5458bdc41b29c5068
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-19 21:33:59 +0530

    done

--- functions/src/FollowFunctions/updateUserInfo.ts
+++ functions/src/FollowFunctions/updateUserInfo.ts
@@ -9,42 +9,21 @@ export const onUserDocUpdate = functions.region('asia-east2').firestore.document
 const upDatedUserData = change.after.data()
 
 const newName = upDatedUserData?.name
-const profilePhotoChosen = upDatedUserData?.profilePhotoChosen
 const updatersUserId = upDatedUserData?.uid
 const newUserName = upDatedUserData?.userName
 
-// const userDoc = change.after.ref.parent
-const followerColl = admin.firestore().collection('Users').doc(updatersUserId).collection('following')
+const userFollowingColl = admin.firestore().collection('Users').doc(updatersUserId).collection('following')
 
-//This is where I am stuck, I have the updated document info but how do
-//I find the other documents at firestore that needs updation with this 
-//updated information of the user
-
-return followerColl.get().then((querySnapshot: { docs: DocumentSnapshot[] }) => {
+return userFollowingColl.get().then((querySnapshot: { docs: DocumentSnapshot[] }) => {
     const promises = querySnapshot.docs.map((doc) => {
-        const followerUid = doc.id
-        return admin.firestore().collection('Users').doc(followerUid).collection('followers')
+        const followeeUid = doc.id
+        return admin.firestore().collection('Users').doc(followeeUid).collection('followers')
         .doc(updatersUserId).set({
             name: newName, 
             userName: newUserName,
-            profilePhotoChosen: profilePhotoChosen,
             uid: updatersUserId
         })
     })
     return Promise.all(promises)
     })
 })
-
-
-
-
-
-//     return admin.firestore()
-//     .collection('Users').doc('{followeeUserId}')
-//     .collection('Followers').doc(updatersUserId)
-//     .set({
-//     name: newName, 
-//     userName: newUserName,
-//     profilePhotoChosen: profilePhotoChosen,
-//     uid: updatersUserId
-//   })
