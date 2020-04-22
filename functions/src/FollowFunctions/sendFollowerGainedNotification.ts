97e494f2e14b8bbb31fcd60a0e573959682d4cd4
Anudeep Ananth <anudeepalliance@gmail.com>
2020-04-22 14:03:59 +0530

    pr

--- functions/src/FollowFunctions/sendFollowerGainedNotification.ts
+++ functions/src/FollowFunctions/sendFollowerGainedNotification.ts
@@ -7,39 +7,44 @@ export const sendTheFollowerGainedNotification = functions.region('asia-east2').
   ('Users/{followerUserId}/following/{followeeUserId}').onCreate((data, context) => {
 
   const followeeUid = context.params.followeeUserId
-  const followerUid : String = context.params.followerUserId
+  const followerUid: String = context.params.followerUserId
+
+  let followerUserName = null
+  let followeeToken = null
 
   //Follower user details that needs to be duplicated to the Followee's following Sub Coll
   admin.firestore().collection('Users').doc(followerUid).get().then((doc:{ exists: any; data: () => any }) => {
 
-    let followerUserName = doc.data().userName
+      followerUserName = doc.data().userName
 
-    admin.firestore().collection('Users').doc(followeeUid).collection('notificationToken')
-      .doc('theNotificationToken').get().then((notificationTokenDoc:{ exists: any; data: () => any }) => {
-
-    let followeeToken =  notificationTokenDoc.data().notificationToken
-    console.log(`followeeToken ${followeeToken}`)
-
-          // Notification details.
-          const payload = {
-            notification: {
-              title: 'You have a new follower!',
-              body: `${followerUserName}`,
-              clickAction: ".People.PersonProfileActivity"
-            },
-            data: {
-              ACTIVITY_NAME: "PersonProfileActivity",
-              PERSON_UID_INTENT_EXTRA: followerUid
-            }
-          }
-
-return admin.messaging().sendToDevice(followeeToken, payload).then(function(response: any) {
-							console.log("Successfully sent message:", response);
-						  })
-						  .catch(function(error: any) {
-							console.log("Error sending message:", error);
-      })                    
     })
+      
+  admin.firestore().collection('Users').doc(followeeUid).collection('notificationToken')
+    .doc('theNotificationToken').get().then((notificationTokenDoc:{ exists: any; data: () => any }) => {
+
+      followeeToken =  notificationTokenDoc.data().notificationToken
+      console.log(`followeeToken ${followeeToken}`)
+
   })
-                
+
+    // Notification details.
+    const payload = {
+        notification: {
+          title: 'You have a new follower!',
+          body: `${followerUserName}`,
+          clickAction: ".People.PersonProfileActivity"
+        },
+        data: {
+          ACTIVITY_NAME: "PersonProfileActivity",
+          PERSON_UID_INTENT_EXTRA: followerUid
+        }
+    }
+    
+  return admin.messaging().sendToDevice(followeeToken, payload).then(function(response: any) {
+          console.log("Successfully sent message:", response);
+      })
+  .catch(function(error: any) {
+      console.log("Error sending message:", error);                
+  })
+
 })
